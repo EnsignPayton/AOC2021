ac23aea38d87193726c68b98ab4c2020dc5e1c1b
Logan Payton <Logan.Payton@javs.com>
2021-12-14 00:52:49 -0500

    Day 12 Refactor

--- AOC2021/Days/Day12.cs
+++ AOC2021/Days/Day12.cs
@@ -1,6 +1,6 @@
 ﻿namespace AOC2021.Days;
 
-public class Day12 : DayBase<Day12.Graph>
+public class Day12 : DayBase<Day12.Pair[]>
 {
     public record Pair(string Node1, string Node2)
     {
@@ -11,142 +11,102 @@ public class Day12 : DayBase<Day12.Graph>
         }
     }
 
-    public record Graph(string[] Vertices, Pair[] Edges)
+    private class GraphVisitor
     {
-        public static Graph Parse(Pair[] data)
-        {
-            var vertices = new List<string>();
+        private const string Start = "start";
+        private const string End = "end";
 
-            foreach (var (node1, node2) in data)
+        private readonly List<string> _visited = new();
+        private readonly List<string> _path = new();
+        private readonly List<List<string>> _paths = new();
+
+        private string _secondVisit = string.Empty;
+
+        public List<List<string>> Search1(Pair[] pairs)
+        {
+            Clear();
+            RecursiveSearch(pairs, Start, x => _visited.Contains(x), _ => { });
+            return _paths.ToList();
+        }
+
+        public List<List<string>> Search2(Pair[] pairs)
+        {
+            bool VertexVisited(string vertex)
             {
-                if (!vertices.Contains(node1))
-                    vertices.Add(node1);
-                if (!vertices.Contains(node2))
-                    vertices.Add(node2);
+                if (_visited.Contains(vertex))
+                {
+                    if (vertex != Start && vertex != End && _secondVisit == string.Empty)
+                        _secondVisit = vertex;
+                    else
+                        return true;
+                }
+
+                return false;
             }
 
-            return new Graph(vertices.ToArray(), data.ToArray());
-        }
-
-        public IEnumerable<string> GetAdjacent(string vertex)
-        {
-            foreach (var (node1, node2) in Edges)
+            void AfterVisit(string curr)
             {
-                if (node1 == vertex)
-                    yield return node2;
-                else if (node2 == vertex)
-                    yield return node1;
-            }
-        }
-    }
-
-    public override long Puzzle1(Graph data)
-    {
-        return CaveSearch1(data).Count;
-    }
-
-    public override long Puzzle2(Graph data)
-    {
-        return CaveSearch2(data).Count;
-    }
-
-    private static List<List<string>> CaveSearch1(Graph graph)
-    {
-        const string start = "start";
-        const string end = "end";
-        var visited = new List<string>();
-        var path = new List<string>();
-        var paths = new List<List<string>>();
-        path.Add(start);
-
-        RecursiveCaveSearch1(graph, start, end, visited, path, paths);
-
-        return paths;
-    }
-
-    private static void RecursiveCaveSearch1(
-        Graph graph,
-        string curr,
-        string dest,
-        ICollection<string> visited,
-        ICollection<string> path,
-        ICollection<List<string>> paths)
-    {
-        if (curr == dest)
-        {
-            paths.Add(path.ToList());
-            return;
-        }
-
-        if (curr.All(char.IsLower))
-            visited.Add(curr);
-
-        foreach (var vertex in graph.GetAdjacent(curr))
-        {
-            if (visited.Contains(vertex)) continue;
-
-            path.Add(curr);
-            RecursiveCaveSearch1(graph, vertex, dest, visited, path, paths);
-            path.Remove(curr);
-        }
-
-        visited.Remove(curr);
-    }
-
-    private static List<List<string>> CaveSearch2(Graph graph)
-    {
-        const string start = "start";
-        const string end = "end";
-        var visited = new List<string>();
-        var path = new List<string>();
-        var paths = new List<List<string>>();
-        path.Add(start);
-
-        var secondVisit = string.Empty;
-        RecursiveCaveSearch2(graph, start, end, visited, path, paths, ref secondVisit);
-
-        return paths;
-    }
-
-    private static void RecursiveCaveSearch2(
-        Graph graph,
-        string curr,
-        string dest,
-        ICollection<string> visited,
-        ICollection<string> path,
-        ICollection<List<string>> paths,
-        ref string secondVisit)
-    {
-        if (curr == dest)
-        {
-            paths.Add(path.ToList());
-            return;
-        }
-
-        if (curr.All(char.IsLower))
-            visited.Add(curr);
-
-        foreach (var vertex in graph.GetAdjacent(curr))
-        {
-            if (visited.Contains(vertex))
-            {
-                if (vertex != "start" && vertex != "end" && secondVisit == string.Empty)
-                    secondVisit = vertex;
-                else
-                    continue;
+                if (_secondVisit == curr)
+                    _secondVisit = string.Empty;
             }
 
-            path.Add(curr);
-            RecursiveCaveSearch2(graph, vertex, dest, visited, path, paths, ref secondVisit);
-            path.Remove(curr);
+            Clear();
+            RecursiveSearch(pairs, Start, VertexVisited, AfterVisit);
+            return _paths.ToList();
         }
 
-        if (secondVisit == curr)
-            secondVisit = string.Empty;
-        visited.Remove(curr);
+        private void Clear()
+        {
+            _visited.Clear();
+            _path.Clear();
+            _paths.Clear();
+            _secondVisit = string.Empty;
+        }
+
+        private void RecursiveSearch(Pair[] pairs, string curr, Func<string, bool> vertexVisited, Action<string> afterVisit)
+        {
+            if (curr == End)
+            {
+                _paths.Add(_path.ToList());
+                return;
+            }
+
+            if (curr.All(char.IsLower))
+                _visited.Add(curr);
+
+            foreach (var vertex in GetAdjacent(pairs, curr))
+            {
+                if (vertexVisited(vertex)) continue;
+
+                _path.Add(curr);
+                RecursiveSearch(pairs, vertex, vertexVisited, afterVisit);
+                _path.Remove(curr);
+            }
+
+            afterVisit(curr);
+            _visited.Remove(curr);
+        }
     }
 
-    protected override Graph Parse(string input) => Graph.Parse(
+    public override long Puzzle1(Pair[] data) =>
+        new GraphVisitor().Search1(data).Count;
+
+    public override long Puzzle2(Pair[] data) =>
+        new GraphVisitor().Search2(data).Count;
+
+    protected override Pair[] Parse(string input) =>
         input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-            .Select(Pair.Parse).ToArray());
+            .Select(Pair.Parse)
+            .ToArray();
+
+    private static IEnumerable<string> GetAdjacent(IEnumerable<Pair> pairs, string vertex)
+    {
+        foreach (var (node1, node2) in pairs)
+        {
+            if (node1 == vertex)
+                yield return node2;
+            else if (node2 == vertex)
+                yield return node1;
+        }
+    }
 }
